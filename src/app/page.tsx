92352d83add9e90d6a9765a9f05941660412f592
Denimar de Moraes <denimar@gmail.com>
2025-06-09 16:45:31 -0300

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PortfolioGrid from "./components/portfolio-grid/PortfolioGrid";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-8 sm:gap-16 font-[family-name:var(--font-geist-sans)] w-full h-full">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-8 sm:gap-16 font-[family-name:var(--font-geist-sans)] w-full h-full bg-gray-50 text-gray-100">
       <main className="flex flex-col gap-6 sm:gap-8 row-start-2 items-center sm:items-start w-full h-full max-w-screen-xl ">
         <PortfolioGrid netLiquidationValue={130000} />
       </main>
