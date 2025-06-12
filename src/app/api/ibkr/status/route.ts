020ba812a9d3aece01961223cf6aaa9651b83365
Denimar de Moraes <denimar@gmail.com>
2025-06-11 21:33:41 -0300

    Initial Commit

--- src/app/api/ibkr/status/route.ts
+++ src/app/api/ibkr/status/route.ts
@@ -2,7 +2,7 @@ import getCookiesFromRequest from "@/lib/getCookiesFromRequest";
 import { ibkrApi } from "@/lib/ibkrApi";
 import { NextRequest, NextResponse } from "next/server";
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+if (process.env.NODE_ENV === 'development') process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export const GET = async (req: NextRequest) => {
   try {
@@ -10,9 +10,14 @@ export const GET = async (req: NextRequest) => {
       endpoint: '/iserver/auth/status',
       cookies: getCookiesFromRequest(req)
     });
+    debugger
     return NextResponse.json(status);
   } catch (e: any) {
+    debugger
     console.error("Error fetching IBKR status:", e.message);
+    if (e.cause) {
+      console.error("Cause:", e.cause.code, e.cause.message);
+    }
     return NextResponse.json({ connected: false, authenticated: false, error: e.message });
   }
 }
